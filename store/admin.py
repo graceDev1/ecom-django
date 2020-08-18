414fad01517e5fc0bd8f36fed4d351658549a88e
fundev <grace.devolop@gmail.com>
2020-08-18 18:52:07 +0200

    datastructure

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Product, ShippingAddress, Order, OrderItem, Customer
 # Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Customer)
--- store/views.py
+++ store/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def store(request):
-    content = {}
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+    content = {'products': products}
+    return render(request, 'store/store.html', content)
 
 
 def cart(request):
-    content = {}
-    return render(request, 'store/cart.html') 
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    content = {'items': items, 'order':order}
+    return render(request, 'store/cart.html', content) 
 
 
 def checkout(request):
-    content = {}
-    return render(request, 'store/checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else: 
+        # it will create an empty for now none-logged in users
+        order = {'get_cart_total': 0, 'get_cart_items':0}
+        items = []
+    content = {'items':items, 'order':order}
+    return render(request, 'store/checkout.html', content)
 
 
